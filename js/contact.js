8172131eb0e7ea25945d933ce78308ea55ef4415
Shubham Bhendarkar <youseeme9182@gmail.com>
2015-06-27 19:47:36 +0530

    contact.html made responsive, links also improved

---
+++ js/contact.js
@@ -0,0 +1,3 @@
+$(window).ready(function() {
+	$('section').css('width', window.innerWidth);
+});
--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,27 @@
 $(document).ready(function(){
+    var flag = 0;
     $("#btn1").click(function(){
-        $("#box").animate({height: "300px"},{duration:400,easing:'swing'});
-        $("#text1").delay(50).animate({left: '240px'},{duration: 300,easing:'swing'});
-        $("#text2").delay(100).animate({left: '150px'});
-        $("#text3").delay(200).animate({left: '150px'});
-        $("#text4").delay(300).animate({left: '150px'});
-        $("#text5").delay(400).animate({left: '210px'});
-        $("#text6").delay(500).animate({left: '150px'});
-        
+        if (flag == 0) {
+            $("#box").animate({height: "300px"},{duration:400,easing:'swing'});
+            $("#text1").delay(50).animate({left: '240px'},{duration: 300,easing:'swing'});
+            $("#text2").delay(100).animate({left: '150px'});
+            $("#text3").delay(200).animate({left: '150px'});
+            $("#text4").delay(300).animate({left: '150px'});
+            $("#text5").delay(400).animate({left: '210px'});
+            $("#text6").delay(500).animate({left: '150px'});
+            $('#btn1').attr('src', 'image/up.png');
+            flag = 1;
+        } else {
 
-    });
-    $("#btn2").click(function(){
-        $("#box").animate({height: "0px"},{duration:900});
-        $("#text1").delay(500).animate({left: '-150px'},{duration: 300,easing:'swing'});
-        $("#text2").delay(400).animate({left: '-150px'});
-        $("#text3").delay(300).animate({left: '-150px'});
-        $("#text4").delay(200).animate({left: '-150px'});
-        $("#text5").delay(100).animate({left: '-190px'});
-        $("#text6").delay(0).animate({left: '-150px'});
+            $("#box").animate({height: "0px"},{duration:900});
+            $("#text1").delay(500).animate({left: '-150px'},{duration: 300,easing:'swing'});
+            $("#text2").delay(400).animate({left: '-150px'});
+            $("#text3").delay(300).animate({left: '-150px'});
+            $("#text4").delay(200).animate({left: '-150px'});
+            $("#text5").delay(100).animate({left: '-190px'});
+            $("#text6").delay(0).animate({left: '-150px'});
+            $('#btn1').attr('src', 'image/down.png');
+            flag = 0;
+        }
     });
 });
