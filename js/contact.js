ede3a59436a4aa2a101a5c4f1f6bbb4edeb09095
Shubham Bhendarkar <youseeme9182@gmail.com>
2015-07-13 11:28:10 +0530

    changes in contact

--- js/contact.js
+++ js/contact.js
@@ -12,7 +12,7 @@ $(function(){
 	$('a.smoothScroll').bind('click',function(event){
 		var $anchor=$(this); 
 		/* if you want to use one of the easing effects: $('html, body').stop().animate({scrollLeft: $($anchor.attr('href')).offset().left}, 1500,'easeInOutExpo'); */ 
-		$('html, body, *').animate({scrollLeft: $anchor.attr('href')[1] * (window.innerWidth + 10)}, 1000); 
+		$('html, body, *').animate({scrollLeft: $anchor.attr('href')[1] * (window.innerWidth)}, 1000); 
 		event.preventDefault();
 	});
 }); 
