6f6005d03798e02e27fbe5e2b2085574b2ef1e58
Saptak Sengupta <saptak.tech193@gmail.com>
2015-07-25 02:25:42 +0530

    version 2 committed

--- js/gallery.js
+++
@@ -1,9 +0,0 @@
-$(window).ready(function() {
-	$('.item img').attr('width', window.innerWidth);
-	$('.item img').attr('height', window.innerHeight);
-});
-
-$(window).resize(function() {
-	$('.item img').attr('width', window.innerWidth);
-	$('.item img').attr('height', window.innerHeight);
-});
